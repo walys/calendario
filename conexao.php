c505e38f7e5871a37c5c73597da4ff0db2b51ac3
walys <walysonfelix1991@gmail.com>
2021-05-03 17:49:28 -0300

    Update conexao

---
+++ conexao.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @author Cesar Szpak - Celke - cesar@celke.com.br
+ * @pagina desenvolvida usando FullCalendar,
+ * o código é aberto e o uso é free,
+ * porém lembre-se de conceder os créditos ao desenvolvedor.
+ */
+
+define('HOST', 'localhost');
+define('USER', 'root');
+define('PASS', '');
+define('DBNAME', 'celke');
+
+$conn = new PDO('mysql:host=' . HOST . ';dbname=' . DBNAME . ';', USER, PASS);
